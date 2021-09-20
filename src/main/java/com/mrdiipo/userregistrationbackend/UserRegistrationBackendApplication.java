73979e92eab857862e5fdd3c4048161ec8bef6b3
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-20 07:16:21 +0100

    Initial commit.

---
+++ src/main/java/com/mrdiipo/userregistrationbackend/UserRegistrationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.userregistrationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrdiipo/userregistrationbackend/UserRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.userregistrationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

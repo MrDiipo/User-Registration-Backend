2201200068ed76be339885a9d9d0ccb6f8a69dbf
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-09-23 20:54:29 +0100

    Email services implementation (Part 2).

--- src/main/java/com/mrdiipo/userregistrationbackend/email/EmailService.java
+++ src/main/java/com/mrdiipo/userregistrationbackend/email/EmailService.java
@@ -2,8 +2,14 @@ package com.mrdiipo.userregistrationbackend.email;
 
 import lombok.AllArgsConstructor;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.logging.Logger;
 
 @Service
@@ -15,11 +21,18 @@ public class EmailService implements EmailSender{
     private final JavaMailSender mailSender;
 
     @Override
+    @Async
     public void send(String to, String email) {
         try {
-                MimeM
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("elegbedeOla@gmail.com");
         } catch (MessagingException e){
-            LOGGER.error()
+            LOGGER.info("failed to send email");
+            throw new IllegalStateException("failed to send email");
         }
     }
 }
